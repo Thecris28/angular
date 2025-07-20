026c6a1e9129f38081975deb9c25bb1103864bab
Thecris28 <sircristian28@gmail.com>
2025-07-20 00:54:40 -0400

    se agrego search y ternding page

--- src/app/gifs/components/side-menu-options/side-menu-options.ts
+++ src/app/gifs/components/side-menu-options/side-menu-options.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 type MenuOption = {
   label: string;
@@ -10,7 +11,7 @@ type MenuOption = {
 
 @Component({
   selector: 'app-side-menu-options',
-  imports: [CommonModule],  // Asegúrate de importar CommonModule
+  imports: [CommonModule, RouterLink, RouterLinkActive],  // Asegúrate de importar CommonModule
   templateUrl: './side-menu-options.html',
   styleUrl: './side-menu-options.css'
 })
@@ -25,7 +26,7 @@ export class SideMenuOptions {
         icon: this.sanitizer.bypassSecurityTrustHtml(`<svg class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
                   <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
                </svg>`),
-        route: 'dashboard/trending'
+        route:'trending'
       },
       {
         label: 'Search',
@@ -33,7 +34,7 @@ export class SideMenuOptions {
       <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
         <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
       </svg>`),
-        route: 'dashboard/search'
+        route:'search'
       },
     ];
   }
